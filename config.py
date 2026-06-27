07057bd0699e3aa22d106a5dfc2c66445c458ae3
KDubov <kirill.dubov@gmail.com>
2026-06-27 20:37:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,4 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# HuggingFace Space URL (ВАЖНО: без https://huggingface.co/)
-HF_SPACE = os.getenv("HF_SPACE")
-
-# если есть токен (не всегда нужен)
-HF_TOKEN = os.getenv("HF_TOKEN")
+HF_SPACE = "KirillDubov/underwater-restoration"
