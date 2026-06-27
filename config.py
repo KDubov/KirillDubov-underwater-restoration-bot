20c71b02728d90769e9cd371c02870f21057ae9a
KDubov <kirill.dubov@gmail.com>
2026-06-27 20:26:47 +0300

    Add HF_SPACE and HF_TOKEN to config
    Added environment variable for HuggingFace Space and optional HF_TOKEN.

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# HuggingFace Space URL (ВАЖНО: без https://huggingface.co/)
+HF_SPACE = os.getenv("HF_SPACE")
+
+# если есть токен (не всегда нужен)
+HF_TOKEN = os.getenv("HF_TOKEN")
